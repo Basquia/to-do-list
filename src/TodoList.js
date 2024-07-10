498ddccb42c4cf856cda3ac998eff60cc459b14f
Basquia <henripaz1998@gmail.com>
2024-07-09 23:44:52 -0300

    fix index

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,14 +2,14 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 function TodoList({ todos, addTodo }) {
-    return (
-        <ul>
-            {todos.map((todo) => (
-                <TodoItem key={todo.text} todo={todo} onDelete={() => { /* Delete funtionality */} } onToggle={() => { /* Toggle completion funtionality */} } />
-            ))}
-            <input type="text" placeholder="add a new todo" onChange={(e) => addTodo(e.target.value)} />
-        </ul>    
-    );
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem key={todo.text} todo={todo} onDelete={() => { /* Delete functionality */} } onToggle={() => { /* Toggle completion functionality */} } />
+      ))}
+      <input type="text" placeholder="Add a new todo" onChange={(e) => addTodo(e.target.value)} />
+    </ul>
+  );
 }
 
 export default TodoList;
